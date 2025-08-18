03e8d6edc70f684d5fd0f7c21f371ab903d71632
Milan Rohit <rohit.milan3@gmail.com>
2025-08-18 08:07:03 +0000

    Update Book and Review models with new relationships and logic

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['review', 'rating', 'book_id'];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
